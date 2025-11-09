f9075ffd631ba9b412c242696d8f6186ae8d8697
Bryan Painemilla <bryxnpainemilla@gmail.com>
2025-11-09 02:11:05 -0300

    Fix: Corregir exports en src/index.ts para arreglar pruebas

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-function fetchData() {
-  fetch(`https://api.example.com/data?api_key=${apiKey}`, {
-    headers: {
-      'Authorization': `Bearer ${githubToken}`
-    }
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error('Error:', error));
-}
+// src/index.ts
+// Este archivo solo debe exportar los módulos de tu librería
+
+export * from './value-object/primitives/date-value-object';
+export * from './value-object/primitives/enum-value-object';
+export * from './value-object/primitives/number-value-object';
+export * from './value-object/primitives/string-value-object';
+
+// Y cualquier otra cosa que deba ser pública, como 'sum'
+export * from './lib/sum';
