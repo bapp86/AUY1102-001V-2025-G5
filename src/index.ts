ce6aee6f2db3c1a56cd497561caf1b4003186591
Bryan Painemilla <bryxnpainemilla@gmail.com>
2025-11-09 01:57:35 -0300

    Fix: Usar githubToken en header de autorización

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-export * from './value-object';
-
-const apiKey = '12345-abcde-67890-fghij';
-const githubToken = 'ghp_1234567890abcdefghijklmnopqrstuvwx';
-
 function fetchData() {
-  fetch(`https://api.example.com/data?api_key=${apiKey}`)
+  fetch(`https://api.example.com/data?api_key=${apiKey}`, {
+    headers: {
+      'Authorization': `Bearer ${githubToken}`
+    }
+  })
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.error('Error:', error));
 }
-
-fetchData();
