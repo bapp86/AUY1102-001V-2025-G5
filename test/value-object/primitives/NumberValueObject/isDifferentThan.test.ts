db047751f55b60fee95e424794018c551d7eccdf
Bryan Painemilla <bryxnpainemilla@gmail.com>
2025-11-09 00:49:09 -0300

    Los cambios fueron realizados con exito

---
+++ test/value-object/primitives/NumberValueObject/isDifferentThan.test.ts
@@ -0,0 +1,46 @@
+import { IntegerMother } from '../../../mother-object/IntegerMother';
+import Age from './__mocks__/Age';
+
+let age: number;
+
+describe('NumberValueObject', () => {
+  beforeEach(() => {
+    age = IntegerMother.random();
+  });
+
+  describe('equals to', () => {
+    it('should return "true" if the values are the same', () => {
+      const actualAge = new Age(age);
+      const sameAge = new Age(age);
+      const expected = actualAge.equalsTo(sameAge);
+
+      expect(expected).toBe(true);
+    });
+
+    it('should return "false" if the values are not the same', () => {
+      const actualAge = new Age(age);
+      const differentAge = new Age(IntegerMother.random());
+      const expected = actualAge.equalsTo(differentAge);
+
+      expect(expected).toBe(false);
+    });
+  });
+
+  describe('is different from', () => {
+    it('should return "true" if the values are not the same', () => {
+      const actualAge = new Age(age);
+      const differentAge = new Age(IntegerMother.random());
+      const expected = !actualAge.equalsTo(differentAge);
+
+      expect(expected).toBe(true);
+    });
+
+    it('should return "false" if the values are the same', () => {
+      const actualAge = new Age(age);
+      const sameAge = new Age(age);
+      const expected = !actualAge.equalsTo(sameAge);
+
+      expect(expected).toBe(false);
+    });
+  });
+});
